6c8d46d03e3cd87cd1d764fc11118709257d9c52
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 13:32:36 +0300

    second

--- cod.py
+++ cod.py
@@ -1,8 +1,5 @@
-s = input()
-l = []
-while s != 'END':
-    num = int(s.split(':')[1])
-    if num > 500:
-        l.append(s.split(':')[0])
-    s = input()
-print(l)
+s = input().split()
+a = input()
+if a in s:
+    s.remove(a)
+print(s)
