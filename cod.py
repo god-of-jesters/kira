40186e254647f44d5b0a8197745cc3b11cf72ed1
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 14:12:42 +0300

    8

--- cod.py
+++ cod.py
@@ -1,5 +1,5 @@
-countries = {}
+internet = {'a': [0, 1], 'b':[0, 0], 'c':[1, 1]}
 n = int(input())
-for i in countries:
-    if len(countries[i]) >= n:
+for i in internet:
+    if internet[i].count(0) > n:
         print(i)
