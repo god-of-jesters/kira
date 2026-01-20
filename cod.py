004fda83ec8fde018ee92360cd281ae56825ab74
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 14:17:36 +0300

    9

--- cod.py
+++ cod.py
@@ -1,5 +1,3 @@
-internet = {'a': [0, 1], 'b':[0, 0], 'c':[1, 1]}
-n = int(input())
-for i in internet:
-    if internet[i].count(0) > n:
-        print(i)
+a = input()
+c = input()
+print(a.count(c))
