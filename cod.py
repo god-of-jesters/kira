c048d5875de9a6502c5f11d316109e08b19b6d86
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 13:17:30 +0300

    task 1

--- cod.py
+++ cod.py
@@ -1,2 +1,14 @@
-sum = 12
-print(sum)
+fd = ['22', '23', '24', '25', '27']
+
+just_mir = 'Это обычная карта МИР'
+no_card = 'Карты мир не бывают с таким большим номером'
+visa_mastercard = 'Возможно, это VISA или MasterCard'
+
+i = input()
+
+if i[:2] in fd and len(i) == 16:
+    print(just_mir)
+elif i[:2] in fd and len(i) > 16:
+    print(no_card)
+else:
+    print(visa_mastercard)
