6b72030b1f7a7fc4fe74d5787ff1c1a3c161c282
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 14:27:03 +0300

    10

--- cod.py
+++ cod.py
@@ -1,7 +1,17 @@
-a = input().split(';')
-c = input()
-s = 0
-for i in a:
-    if i.split('|')[2] == c:
-        s += 1
-print(s)
+s = input()
+l = {}
+m = 0
+g = ''
+while s != 'END':
+    gr = s.split(', ')[0]
+    if gr not in l:
+        l[gr] = 0
+    date = s.split(', ')[1]
+    if date.split('.')[-1] == '2023':
+        l[gr] += 1
+    s = input()
+for i in l:
+    if l[i] > m:
+        m = l[i]
+        g = i
+print(g, m)
