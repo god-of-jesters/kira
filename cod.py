b3265fd9d07c3e6112063e3633bd6b81898905bd
god-of-jesters <ryzovdima@gmail.com>
2026-01-20 13:26:07 +0300

    second

--- cod.py
+++ cod.py
@@ -1,14 +1,8 @@
-fd = ['22', '23', '24', '25', '27']
-
-just_mir = 'Это обычная карта МИР'
-no_card = 'Карты мир не бывают с таким большим номером'
-visa_mastercard = 'Возможно, это VISA или MasterCard'
-
-i = input()
-
-if i[:2] in fd and len(i) == 16:
-    print(just_mir)
-elif len(i) > 16:
-    print(no_card)
-else:
-    print(visa_mastercard)
+s = input()
+l = []
+while s != 'END':
+    num = int(s.split(':')[1])
+    if num > 500:
+        l.append(s.split(':')[0])
+    s = input()
+print(l)
