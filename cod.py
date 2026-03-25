21d98fe1e23b712c53b064ea7c34f352fff83486
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 16:41:01 +0300

    b

--- cod.py
+++ cod.py
@@ -1,13 +1,1 @@
-#6
-to_buy = 'покупаем'
-not_to_buy = 'Седня без покупочек'
-a = input()
-b = int(input())
-s = 0
-for i in a.split(', '):
-    s += int(i.split(': ')[1])
-
-if b >= s:
-    print(to_buy)
-else:
-    print(not_to_buy)
+#7  Ответ 2,4,5
