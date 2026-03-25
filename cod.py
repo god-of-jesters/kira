d90565177d73998f04aae6a0f39964485bd6b219
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 17:21:58 +0300

    b

--- cod.py
+++ cod.py
@@ -1,6 +1,6 @@
-x = int(input())
-
 f = open("markets_data.csv", "r", encoding="utf-8")
+
+cities = {}
 
 for line in f:
     s = line.strip()
@@ -9,7 +9,19 @@ for line in f:
     city = parts[0]
     money = int(parts[3])
 
-    if city == "Москва" and money > x:
-        print(s)
+    if city in cities:
+        cities[city] += money
+    else:
+        cities[city] = money
 
 f.close()
+
+max_city = ""
+max_money = -1
+
+for city in cities:
+    if cities[city] > max_money:
+        max_money = cities[city]
+        max_city = city
+
+print(max_city)
