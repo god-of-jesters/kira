33ed377d6131c99b9e0157d2952f7dbdf596d9ad
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 17:02:26 +0300

    b

--- cod.py
+++ cod.py
@@ -1,12 +1,11 @@
-a = input().split(', ')
-b = input().split(', ')
-c = input().split(', ')
-r = []
-for i in a:
-    if i in b and i in c and i not in r:
-        r.append(i)
-
-r.sort(reverse=True)
-
-if len(r) > 0:
-    print(', '.join(r))
+markets = {
+    'a': ['1', '2'],
+    'b': ['1', '409'],
+    'c': ['12', '2']
+}
+a = input()
+c = 0
+for n, i in markets.items():
+    if a in i:
+        c += 1
+print(c)
