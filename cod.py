be143b3957c2c4f31cc6b00568823abc84a6c3b7
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 16:56:38 +0300

    b

--- cod.py
+++ cod.py
@@ -1,1 +1,12 @@
-#7  Ответ 2,4,5
+a = input().split(', ')
+b = input().split(', ')
+c = input().split(', ')
+r = []
+for i in a:
+    if i in b and i in c and i not in r:
+        r.append(i)
+
+r.sort(reverse=True)
+
+if len(r) > 0:
+    print(', '.join(r))
