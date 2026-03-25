eeece379105479b68d643f77eef880f0a8573b55
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 16:36:22 +0300

    b

--- cod.py
+++ cod.py
@@ -1,9 +1,13 @@
-#5 
+#6
+to_buy = 'покупаем'
+not_to_buy = 'Седня без покупочек'
 a = input()
-c = 0
-while a != 'конец':
-    b = a.split(', ')
-    if b[1] == 'посетила':
-        c += 1
-    a = input()
-print(c)
+b = int(input())
+s = 0
+for i in a.split(', '):
+    s += int(i.split(': ')[1])
+
+if b >= s:
+    print(to_buy)
+else:
+    print(not_to_buy)
