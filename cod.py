98fcfe1b7abcba5d34734cb6526f50c706098ec4
god-of-jesters <ryzovdima@gmail.com>
2026-03-25 16:24:59 +0300

    b

--- cod.py
+++ cod.py
@@ -1,2 +1,12 @@
 #1  Ответ 3 и 4 
 #2  Ответ 1, 2, 3
+#3  Ответ 2, 3
+#4 
+to_buy = 'Покупаем!'
+not_to_buy = 'К сожалению, сегодня без покупок'
+a = input()
+b = input().split(', ')
+if b[1] == a:
+    print(to_buy)
+else:
+    print(not_to_buy)
